652bd86ce202ff73232350741f687120fc7cbe60
Tim Stelter <tstelter@vt.edu>
2018-05-28 13:01:32 -0400

    Added BitVector practice class

---
+++ CodePracticeJava/src/BitVectors.java
@@ -0,0 +1,20 @@
+import java.util.BitSet;
+
+/**
+ * Practice using BitSet class
+ * 
+ * @author Tim Stelter
+ *
+ */
+public class BitVectors {
+
+	public static void main(String[] args) {
+		BitSet bitset = new BitSet();
+		
+		bitset.set(0011);
+		
+		System.out.println(bitset);
+
+	}
+
+}
